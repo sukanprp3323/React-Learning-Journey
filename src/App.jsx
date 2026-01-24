446c1e01b3fbd053ef068400d2d2d1c2ab9cd181
Sukan Prajapati <sukanprajapati3323@gmail.com>
2026-01-24 12:22:04 +0530

    shortCircuit.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 
 //* new way using React JS
 
-import { RegistarationForm } from "./Projects/RegistrationForm/registaration";
+import ShortCircuitExample from "./Projects/ShortCircuit/ShotCircuit";
+
+
+
+// import { Keys } from "./keys";
+
+// import { RegistarationForm } from "./Projects/RegistrationForm/registaration";
 
 // import { ToDO } from "./Projects/ToDO Project/ToDo";
 
 export const App = () => {
   return (
     <section>
+    <ShortCircuitExample/>  
+    {/* <Keys/> */}
       {/* <ToDO/> */}
-      <RegistarationForm />
+      {/* <RegistarationForm /> */}
     </section>
   )
 };
---
+++ src/Projects/ShortCircuit/ShotCircuit.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "../ShortCircuit/shortCUR.css"
+
+const ShortCircuitExample = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [user, setUser] = useState("")
+    return (
+        <>
+            <section className="container short-container">
+                <h1 className="shortCircuitHeading">Welcome to the ShortCircuit Evaluation!</h1>
+
+                {/* conditional redering using short circuit evalution */}
+                {isLoggedIn && <p className="status">You are logged IN!</p>}
+
+                {/* For USER Greeting with Name*/}
+                <p className="status"> {user ? `Hello, ${user}` : "Hello, User pls Login"}</p>
+
+                <div className="grid-three-cols">
+                    <button onClick={() => { setIsLoggedIn(!isLoggedIn) }}>Togggle Login Status</button>
+                    <button onClick={() => { setUser("Sukan Prajapati") }}>Set User</button>
+                    <button onClick={() => setUser("")}>Clear User</button>
+                </div>
+            </section>
+        </>
+    )
+};
+
+export default ShortCircuitExample;
